992e622f458483f771df02bb3055c47a937dda8e
Bhargavi <sesettybhargavi@gmail.com>
2026-04-15 13:50:52 +0530

    completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
 
+function PageLayout({ header, footer, children }) {
+  return (
+    <div>
+      {header}
+      {children}
+      {footer}
+    </div>
+  );
+}
 
+const App = () => {
+  return (
+    <PageLayout
+      header={<h1>Welcome to my website</h1>}
+      footer={<p>Footer here</p>}
+    >
+      <p>This is the content of my website.</p>
+    </PageLayout>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
