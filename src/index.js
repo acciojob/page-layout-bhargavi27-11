a142291a080ef847d1dae55346134ef41201293f
Bhargavi <sesettybhargavi@gmail.com>
2026-04-24 13:58:30 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <PageLayout
       header={<h1>Welcome to my website</h1>}
-      footer={<p>Footer here</p>}
+      footer={<p>@ 2023 My Website. All rights reserved.</p>}
     >
       <p>This is the content of my website.</p>
     </PageLayout>
